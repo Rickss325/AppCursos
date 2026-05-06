7b558a36ec20e5dacbabcaf82eea3fbf85b9585e
Rickss325 <lopezlopezricardo329@gmail.com>
2026-05-06 10:16:13 -0600

    Add curso_1.py

---
+++ curso_1.py
@@ -0,0 +1,11 @@
+"""
+Programa: curso_1
+Descripción: Programa de prueba para el curso
+"""
+
+def main():
+    print("¡Bienvenido al programa curso_1!")
+    print("Este es un programa de ejemplo.")
+
+if __name__ == "__main__":
+    main()
