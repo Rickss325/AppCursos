a3a8a717286f3467a37ebb944501b768227f1268
Noe-23 <noe888170@gmail.com>
2026-05-11 08:14:31 -0600

    Update curso_1.py

--- curso_1.py
+++ curso_1.py
@@ -1,11 +1,5 @@
-"""
-Programa: curso_1
-Descripción: Programa de prueba para el curso
-"""
-
 def main():
-    print("¡Bienvenido al programa curso_1!")
-    print("Este es un programa de ejemplo.")
+    print("¡Bienvenido a CursoApps!!!")
 
 if __name__ == "__main__":
     main()
